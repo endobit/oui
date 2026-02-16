12ceb2466fb9557b5158d5fbce4b3dcfe0052445
Mason Katz <mjk@endobit.io>
2026-02-15 17:35:16 -0800

    refactor: use constants and let the compiler optimize
    Compile will fold duplicate constants no need to have our own map. Motivation is
    to make a rebuild of data.go be comparable to the previous one. Before this a
    single vendor change could affect most of the file.

--- oui.go
+++ oui.go
@@ -18,41 +18,38 @@ import (
 // not found. The value s can be either just the OUI or a mac address,
 // it can also include or exclude the ":" delimiters. Any of the
 // following are valid inputs:
-//     0A:0B:0C
-//     0a:0b:0c
-//     0A0B0C
-//     0a0b0c
-//     0A:0B:0C:01:02:03
-//     0a:0b:0c:01:02:03
-//     0A0B0C010203
-//     0a0b0c010203
-func Vendor(s string) string {
-	s = strings.ReplaceAll(s, ":", "")
+//
+//	0A:0B:0C
+//	0a:0b:0c
+//	0A0B0C
+//	0a0b0c
+//	0A:0B:0C:01:02:03
+//	0a:0b:0c:01:02:03
+//	0A0B0C010203
+//	0a0b0c010203
+func Vendor(addr string) string {
+	addr = strings.ReplaceAll(addr, ":", "")
 
 	switch {
-	case len(s) < 6:
+	case len(addr) < 6:
 		return ""
-	case len(s) > 6:
-		s = s[0:6]
+	case len(addr) > 6:
+		addr = addr[0:6]
 	}
 
-	s = strings.ToLower(s)
+	addr = strings.ToLower(addr)
 
-	id, ok := ouis[s]
+	vendor, ok := ouis[addr]
 	if !ok {
 		return ""
 	}
 
-	if id > len(vendors) {
-		return ""
-	}
-
-	return vendors[id]
+	return vendor
 }
 
 // VendorFromMAC returns the hardware vendor of a net.HardwareAddr.
 // Both 48 and 64 bits addresses are valid, as the OUI is the first 24
 // bits of both.
-func VendorFromMAC(hw net.HardwareAddr) string {
-	return Vendor(hw.String())
+func VendorFromMAC(mac net.HardwareAddr) string {
+	return Vendor(mac.String())
 }
